33e06eb9b5761ef454d716072bbd971da5d88c15
Nathanon-Jaem <kumch29@gmail.com>
2026-01-09 21:09:12 +0700

    Implement statistical calculations in lab13_1.cpp
    Add statistical functions to calculate mean, standard deviation, product, inverse sum, max, and min.

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,28 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double a[], int n, double b[]) {
+    double sum = 0;
+    double sumSq = 0;
+    double product = 1.0;
+    double sumInv = 0;
+    double maxVal = a[0];
+    double minVal = a[0];
+
+    for (int i = 0; i < n; i++) {
+        sum += a[i];
+        sumSq += a[i] * a[i];
+        product *= a[i];
+        sumInv += 1.0 / a[i];
+        if (a[i] > maxVal) maxVal = a[i];
+        if (a[i] < minVal) minVal = a[i];
+    }
+
+    b[0] = sum / n;
+    b[1] = sqrt((sumSq / n) - (b[0] * b[0]));
+    b[2] = pow(product, 1.0 / n);
+    b[3] = n / sumInv;
+    b[4] = maxVal;
+    b[5] = minVal;
+}
